4fc1e549f3790a64d80be1b8ba4d5736edd7f034
martinvoegele <martinvoegele1989@gmail.com>
2021-05-17 21:27:59 -0700

    proper subpackaging

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(name='gpcrmining',
-      version='0.1.2',
+      version='0.1.3',
       description='Functions to scrape GPCR data from the web.',
       url='http://github.com/drorlab/GPCR-mining',
       author='Martin Voegele',
       author_email='mvoegele@stanford.edu',
       license='MIT',
-      packages=['gpcrmining'],
+      packages=find_packages(include=[
+        'gpcrmining',
+        'gpcrmining.gpcrdb',
+      ]),
       zip_safe=False,
       install_requires=[
         'numpy',
